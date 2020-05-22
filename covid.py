028dc5c681a72685a3d333f41dab84e6a22596f3
kk289 <kevilkhadka@gmail.com>
2020-05-21 22:01:30 -0700

    Updating automatic git pull'

--- covid.py
+++ covid.py
@@ -10,7 +10,7 @@ import seaborn as sns
 world_df = pd.read_csv('https://raw.githubusercontent.com/bitprj/covid19/master/covid_data.csv',parse_dates=['Date'])
 
 #Display top 5 records
-print(world_df.head(5))
+print(world_df.head(10))
 
 #view some basic statistical details like percentile, mean, std 
 world_df.describe()
@@ -20,12 +20,17 @@ world_df.info()
 #whether the data frame has any NAN values
 world_df.isnull().values.any()
 
+# Preprocessing the dataset
 #Replace NAN values with blank string
 world_df.fillna({'Province_State': ""},inplace=True)
 
 #Drop unusefull columns if any 
 world_df.drop("Id",axis=1,inplace=True)
 
+# Data Visualization
 #bar chart for total confirmed and fatalities cases
 axis = world_df[['Date','ConfirmedCases','Fatalities']].set_index('Date').plot(figsize=(12, 8),logy=True)
 plt.show()
+
+# Data Aggregation
+world_df.groupby('Country_Region')
