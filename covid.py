8047d2f7ac58b220f340ca4613718e18d140cf42
kk289 <kevilkhadka@gmail.com>
2020-05-21 20:55:31 -0700

    Added Dataset

--- covid.py
+++ covid.py
@@ -4,4 +4,11 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from fbprophet import Prophet
 import seaborn as sns
-# matplotlib inline 
+# %matplotlib inline
+
+#Load the csv data, creating pandas dataframe by persisting date format
+world_df = pd.read_csv('https://raw.githubusercontent.com/bitprj/covid19/master/covid_data.csv',parse_dates=['Date'])
+
+#Display top 5 records
+record = world_df.head(5)
+print(record)
