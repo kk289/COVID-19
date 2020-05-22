802cf59bab27caf113d54476399948a949c97c2b
kk289 <kevilkhadka@gmail.com>
2020-05-21 21:57:14 -0700

    Update file

--- covid.py
+++ covid.py
@@ -10,5 +10,22 @@ import seaborn as sns
 world_df = pd.read_csv('https://raw.githubusercontent.com/bitprj/covid19/master/covid_data.csv',parse_dates=['Date'])
 
 #Display top 5 records
-record = world_df.head(5)
-print(record)
+print(world_df.head(5))
+
+#view some basic statistical details like percentile, mean, std 
+world_df.describe()
+
+world_df.info()
+
+#whether the data frame has any NAN values
+world_df.isnull().values.any()
+
+#Replace NAN values with blank string
+world_df.fillna({'Province_State': ""},inplace=True)
+
+#Drop unusefull columns if any 
+world_df.drop("Id",axis=1,inplace=True)
+
+#bar chart for total confirmed and fatalities cases
+axis = world_df[['Date','ConfirmedCases','Fatalities']].set_index('Date').plot(figsize=(12, 8),logy=True)
+plt.show()
